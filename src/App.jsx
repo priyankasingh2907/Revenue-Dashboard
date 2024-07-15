4aa0dcb102f6e0b213f595e76ac569ec087818cc
priyanka singh <priyanka.singh.2907.2k.com>
2024-07-15 21:09:25 +0530

    complted

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import styled from "styled-components";
+import Sidebar from "./Components/Sidebar";
+import MainContent from "./Components/MainContent";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <Container>
+      <Sidebar />
+      <MainContent />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  height: 97vh;
+  background: linear-gradient(to bottom, white 0%, #e6e4ff);
+  border-radius: 2rem;
+`;
